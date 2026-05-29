a77a3deb54b6109eddfef6fe794766a4013159cf
Eduardo Vieira <eduardo.ro.vieira@gmail.comm>
2026-05-29 18:26:57 -0300

    modo darkmode implementado, assim como a paleta de cores foi alterada para um melhor conforto visual tanto no dark mode quanto no light mode

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
-//icones de play e pause em SVG
+//icones em SVG
 const pauseIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="M525-200v-560h235v560H525Zm-325 0v-560h235v560H200Zm385-60h115v-440H585v440Zm-325 0h115v-440H260v440Zm0-440v440-440Zm325 0v440-440Z"/></svg>`;
 const playIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="M323.5-208.5v-549l431 274.5-431 274.5ZM381-483Zm0 170 267.5-170L381-653v340Z"/></svg>`;
+const lightModeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M565-395q35-35 35-85t-35-85q-35-35-85-35t-85 35q-35 35-35 85t35 85q35 35 85 35t85-35Zm-226.5 56.5Q280-397 280-480t58.5-141.5Q397-680 480-680t141.5 58.5Q680-563 680-480t-58.5 141.5Q563-280 480-280t-141.5-58.5ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>`;
+const darkModeIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`;
 
 //mixers
 const players =[document.querySelector('#audio1'), document.querySelector('#audio2')];
@@ -186,7 +188,19 @@ function cardAtivo(card) {
 //darkmode
 
 darkModeButton.addEventListener('click', () => {
-document.querySelector('body').classList.toggle('dark');
-document.querySelector('#header').classList.toggle('dark');
-document.querySelectorAll('.card').forEach(card => card.classList.toggle('dark'));   
+    document.querySelector('body').classList.toggle('dark');
+    document.querySelector('nav').classList.toggle('dark');
+    document.querySelector('p').classList.toggle('dark');
+    document.querySelector('footer').classList.toggle('dark');
+    document.querySelector('#theme-button').classList.toggle('dark');
+    document.querySelector('#header').classList.toggle('dark');
+    document.querySelectorAll('.card').forEach(card => card.classList.toggle('dark'));
+
+    atualizarIconeTema();
 });
+
+function atualizarIconeTema() {
+    const isDarkMode = document.querySelector("#theme-button").classList.contains("dark");
+    darkModeButton.innerHTML = isDarkMode ? lightModeIcon : darkModeIcon;
+};
+atualizarIconeTema();
